3a883868fc81e15c9126c6e28f16d7bd5977f09d
yangsy02 <eureka7655@naver.com>
2024-03-20 10:36:33 +0900

    [0320] 10809번

--- CLASS1/1157.py
+++ CLASS1/1157.py
@@ -1,13 +1,13 @@
 word = input().upper()
-word_list = list(set(word))
+word_list = list(set(word)) #중복 제거해서 리스트에 넣기
 
-cnt = []
+cnt = [] #각 문자가 쓰여진 횟수를 저장하는 변수 
 for i in word_list:
-  count = word.count
-  cnt.append(count(i))
+  count = word.count #conut에 word.count함수 할당
+  cnt.append(count(i)) #HELLO라면 H가 1번 이니까 cnt에 1추가 
 
-if cnt.count(max(cnt)) > 1:
+if cnt.count(max(cnt)) > 1: #가장 많은 cnt가 여러개라면 물음표 출력
   print("?")
 
 else:
-  print(word_list[(cnt.index(max(cnt)))])
+  print(word_list[(cnt.index(max(cnt)))]) 
